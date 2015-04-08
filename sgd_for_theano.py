815a633ce1a12f0ee0500ec15c9ee002b1b01507
llcao <liangliang.cao@gmail.com>
2015-04-08 15:11:23 -0400

    add a comment to get_update_rmsprop.py

--- sgd_for_theano.py
+++ sgd_for_theano.py
@@ -16,8 +16,9 @@ def gen_updates_sgd(loss, all_parameters, learning_rate):
     for param_i, grad_i in zip(all_parameters, all_grads):
         updates.append((param_i, param_i - learning_rate * grad_i))
     return updates
-
+	
 def get_update_rmsprop(loss, all_parameters, learning_rate):
+	# this function is copied from Chad DeChant's implementation
     all_grads = T.grad(loss, all_parameters)
     updates = []
     for p, g in zip(all_parameters, all_grads):
